26745238f049e25e45ca4a68bb4a7f56dd68a1de
ascrm <2339621373@qq.com>
2025-03-16 14:23:25 +0800

    cn-travel-api first commit

--- src/main/java/com/travel/CnTravelApiApplication.java
+++ src/main/java/com/travel/CnTravelApiApplication.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.travel")
+@SpringBootApplication
 @Slf4j
 public class CnTravelApiApplication {
     public static void main(String[] args) {
